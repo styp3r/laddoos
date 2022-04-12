eded6f2f67466a47de15eb2b26905b97fb63c494
Navjyot Jaiswal <stypertypes@gmail.com>
2022-04-12 19:35:56 +0530

    Creates About Us Section with bg art and adds new images to products section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Image from '../images/image.gif';
+import Sweets from '../images/sweets.jpg';
+import Chaats from '../images/chaats.jpg';
+import Combo from '../images/combo.jpg';
+import Namkeen from '../images/namkeen.jpg';
+import Paratha from '../images/paratha.jpg';
+import Beverages from '../images/badamMilk.JPG';
 import ItemBox from './ItemBox';
+import Bg from '../images/bgSubs.png';
 
 function Home(){
     return(
        <div id = "homePage">
+
         <div id = "heroSection">
             <div className = "heroText">
                 <div className = "heroTextContent">
@@ -19,6 +27,7 @@ function Home(){
                 </div>
             </div>
         </div>
+
         <div id = "productsSection">
             <div className = "productsText">
                 <div className = "prodTextContent">
@@ -28,14 +37,21 @@ function Home(){
             </div>
             <div className = "productDisplay">
                 <div className = "prodListContainer">
-                    <ItemBox img = "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg" name = "samosa1"/>
-                    <ItemBox img = "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg" name = "samosa2"/>
-                    <ItemBox img = "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg" name = "samosa3"/>
-                    <ItemBox img = "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg" name = "samosa4"/>
-                    <ItemBox img = "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg" name = "samosa5"/>
-                    <ItemBox img = "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg" name = "samosa6"/>
+                    <ItemBox img = {Sweets} name = "Sweets"/>
+                    <ItemBox img = {Chaats} name = "Chaats & Snacks"/>
+                    <ItemBox img = {Namkeen} name = "Namkeen"/>
+                    <ItemBox img = {Paratha} name = "Parathas & Puris"/>
+                    <ItemBox img = {Combo} name = "Special Combos"/>
+                    <ItemBox img = {Beverages} name = "Beverages"/>
                 </div>
+                <button className = "viewAllBtn">View All</button>
             </div>
+        </div>
+
+        <div id = "aboutSection">
+            <img className = "bg" src = {Bg} />
+            <h3 className = "homeAboutHeading">ABOUT US</h3>
+            <p className = "homeAboutText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         </div>
     );
