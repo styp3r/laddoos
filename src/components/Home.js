ba0f9d4f12f58940027eba94cfdb8da10e350f06
Navjyot Jaiswal <stypertypes@gmail.com>
2022-04-12 21:16:47 +0530

    Creates a WhyUs section with listed benefits and a feature image

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import Paratha from '../images/paratha.jpg';
 import Beverages from '../images/badamMilk.JPG';
 import ItemBox from './ItemBox';
 import Bg from '../images/bgSubs.png';
+import Feature from '../images/featureImg.jpg';
 
 function Home(){
     return(
@@ -52,6 +53,28 @@ function Home(){
             <img className = "bg" src = {Bg} />
             <h3 className = "homeAboutHeading">ABOUT US</h3>
             <p className = "homeAboutText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+
+        <div id = "whyUsSection">
+            <div className = "whyUsText">
+                <div className = "whyUsTextContent">
+                    <div className = "whySubText1">WHY US?</div>
+                    <div className = "whySubText2">It's Not Just About The Food, It's About The Experience</div>
+                    <img className = "featureImg" src = {Feature}/>
+                </div>
+            </div>
+            <div className = "benefitsList">
+                <div className = "benefitsListContainer">
+                <ul className = "listOfbenefits">
+                    <li className = "benefitHeading1">&#x1F4AF; Best Quality</li>
+                    <li className = "benefitText1">We try to make products of the best quality so that you can enjoy true, authentic and tasty Indian food.</li>
+                    <li className = "benefitHeading2">&#x1F957; Large Selection Of Products</li>
+                    <li className = "benefitText2">We have an inventory of more than a 100 products and the list keeps growing so there is always something for everyone.</li>
+                    <li className = "benefitHeading3">&#x1F44D; Customer Satisfaction</li>
+                    <li className = "benefitText3">We serve 1000+ happy customers on a daily basis and we always acknowledge and work on feedbacks so that we can provide you with the best possible experience!</li>
+                </ul>
+                </div>
+            </div>
         </div>
         </div>
     );
