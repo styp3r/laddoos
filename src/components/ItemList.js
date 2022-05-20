753f01b1667f88f0f0536e951a6c37c9aeeb6604
Navjyot Jaiswal <stypertypes@gmail.com>
2022-05-20 23:16:18 +0530

    Adds item images and uploads images and item data to ItemList.js - snacks, chaats and beverages

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,32 +1,45 @@
-import vadaPav from '../images/Items/vada pav.jpg';
+//Snacks
+import vadaPav from '../images/Items/Snacks/vada pav.jpg';
+import tikkiyaPav from '../images/Items/Snacks/tikkiya pav.jpg';
+import samosaPav from '../images/Items/Snacks/samosa pav.jpg';
+import cutletPav from '../images/Items/Snacks/cutlet pav.jpg';
+import cheeseVadaPav from '../images/Items/Snacks/cheese vada pav.jpg';
+import paniPuri from '../images/Items/Snacks/pani puri.jpg';
+//Chaats
+import alooBatata from '../images/Items/chaats/aloo batata.jpg';
+import bhelPuri from '../images/Items/chaats/bhel puri.jpg';
+import cutletChat from '../images/Items/chaats/cutlet chat.jpg';
+import cutletMutter from '../images/Items/chaats/cutlet mutter.jpg';
+import dahiBatata from '../images/Items/chaats/dahi batata.jpg';
+import dahiKatoriChat from '../images/Items/chaats/dahi katori chat.jpg';
+import dahiPapdi from '../images/Items/chaats/dahi papdi.jpg';
+import dahiRajKachori from '../images/Items/chaats/dahi raj kachori.jpg';
+import jhalMuri from '../images/Items/chaats/jhal muri.jpg';
+import kachoriChaat from '../images/Items/chaats/kachori chaat.jpg';
+import kachoriMutter from '../images/Items/chaats/kachori mutter.jpg';
+import masalaPuri from '../images/Items/chaats/masala puri.jpg';
+import papdiChole from '../images/Items/chaats/papdi chole.jpg';
+import papdiMutter from '../images/Items/chaats/papdi mutter.jpg';
+import plainMutter from '../images/Items/chaats/plain mutter.jpg';
+import rasgullaChat from '../images/Items/chaats/rasgulla chat.jpg';
+import samosaChat from '../images/Items/chaats/samosa chat.jpg';
+import samosaMutter from '../images/Items/chaats/samosa mutter.jpg';
+import sevPuri from '../images/Items/chaats/sev puri.jpg';
+import tikiyaMutter from '../images/Items/chaats/tikiya mutter.jpg';
+import tomatoChaat from '../images/Items/chaats/tomato chaat.jpg';
+//Beverages
+import badamMilk from '../images/Items/Beverages/badam milk.jpg';
+import chaas from '../images/Items/Beverages/chaas.jpg';
+import hotBadamMilk from '../images/Items/Beverages/hot badam milk.jpg';
+import mangoLassi from '../images/Items/Beverages/mango lassi.jpg';
+import saltLassi from '../images/Items/Beverages/salt lassi.jpg';
+import sweetLassi from '../images/Items/Beverages/sweet lassi.jpg';
 //import from '../images/Items/';
-import laddoo from '../images/Items/Ghee Motichoor Laddoo.jpg';
-import lassi from '../images/Items/sweet lassi.jpg';
-import papdi from '../images/Items/papdi.jpg';
-import mysorePak from '../images/Items/mysore pak.jpg';
 
 
 const ItemList = [
     {
-        id: 1,
-        name: "Samosa",
-        category: "snacks",
-        bestseller: true,
-        qty: "1 Piece",
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
-    },
-    {
-        id: 2,
-        name: "Item2",
-        category: "snacks",
-        bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
-    },
-    {
-        id: 3,
+        id: 0,
         name: "Vada Pav",
         category: "snacks",
         bestseller: true,
@@ -35,121 +48,292 @@ const ItemList = [
         img: vadaPav
     },
     {
-        id: 4,
-        name: "Item4",
+        id: 1,
+        name: "Tikkiya Pav",
         category: "snacks",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Piece",
+        price: 46,
+        img: tikkiyaPav
+    },
+    {
+        id: 2,
+        name: "Samosa Pav",
+        category: "snacks",
+        bestseller: false,
+        qty: "1 Piece",
+        price: 46,
+        img: samosaPav
+    },
+    {
+        id: 3,
+        name: "Cutlet Pav",
+        category: "snacks",
+        bestseller: false,
+        qty: "1 Piece",
+        price: 46,
+        img: cutletPav
+    },
+    {
+        id: 4,
+        name: "Cheese Vada Pav",
+        category: "snacks",
+        bestseller: false,
+        qty: "1 Piece",
+        price: 50,
+        img: cheeseVadaPav
     },
     {
         id: 5,
-        name: "Motichoor Laddoo",
-        category: "sweets",
-        bestseller: true,
-        qty: "250gms",
-        price: 151,
-        img: laddoo
+        name: "Pani Puri",
+        category: "snacks",
+        bestseller: false,
+        qty: "6 Pieces",
+        price: 45,
+        img: paniPuri
     },
     {
         id: 6,
-        name: "Item6",
-        category: "snacks",
+        name: "Aloo Batata Puri",
+        category: "chaats",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Plate",
+        price: 60,
+        img: alooBatata
     },
     {
         id: 7,
-        name: "Rasgulla",
-        category: "sweets",
-        bestseller: true,
-        qty: "6 Pieces",
-        price: 175,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        name: "Bhel Puri",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 55,
+        img: bhelPuri
     },
     {
         id: 8,
-        name: "Mysore Pak",
-        category: "sweets",
-        bestseller: true,
-        qty: "250gms",
-        price: 175,
-        img: mysorePak
+        name: "Cutlet Chaat",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 75,
+        img: cutletChat
     },
     {
         id: 9,
-        name: "Gujarati Papdi",
-        category: "namkeen",
-        bestseller: true,
-        qty: "180gms",
-        price: 80,
-        img: papdi
+        name: "Cutlet Mutter",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 75,
+        img: cutletMutter
     },
     {
         id: 10,
-        name: "Item10",
-        category: "snacks",
+        name: "Dahi Batata Puri",
+        category: "chaats",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Plate",
+        price: 80,
+        img: dahiBatata
     },
     {
         id: 11,
-        name: "Kaju Burfi",
-        category: "sweets",
-        bestseller: true,
-        qty: "250gms",
-        price: 275,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        name: "Dahi Katori Chaat",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 85,
+        img: dahiKatoriChat
     },
     {
         id: 12,
-        name: "Item12",
-        category: "snacks",
+        name: "Dahi Papdi Chaat",
+        category: "chaats",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Plate",
+        price: 80,
+        img: dahiPapdi
     },
     {
         id: 13,
-        name: "Item13",
-        category: "snacks",
+        name: "Dahi Raj Kachori",
+        category: "chaats",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Plate",
+        price: 85,
+        img: dahiRajKachori
     },
     {
         id: 14,
-        name: "Item14",
-        category: "snacks",
+        name: "Jhal Muri",
+        category: "chaats",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Plate",
+        price: 60,
+        img: jhalMuri
     },
     {
         id: 15,
-        name: "Sweet Lassi",
-        category: "beverages",
-        bestseller: true,
-        qty: "150ml",
-        price: 65,
-        img: lassi
+        name: "Kachori Chaat",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 72,
+        img: kachoriChaat
     },
     {
         id: 16,
-        name: "Item16",
-        category: "snacks",
+        name: "Kachori Mutter",
+        category: "chaats",
         bestseller: false,
-        qty: 1,
-        price: 25,
-        img: "https://www.cubesnjuliennes.com/wp-content/uploads/2020/08/Best-Mutton-Keema-Samosa-Recipe-1.jpg"
+        qty: "1 Plate",
+        price: 75,
+        img: kachoriMutter
+    },
+    {
+        id: 17,
+        name: "Masala Puri",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 55,
+        img: masalaPuri
+    },
+    {
+        id: 18,
+        name: "Papdi Chole",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 75,
+        img: papdiChole
+    },
+    {
+        id: 19,
+        name: "Papdi Mutter",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 75,
+        img: papdiMutter
+    },
+    {
+        id: 20,
+        name: "Plain Mutter",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 55,
+        img: plainMutter
+    },
+    {
+        id: 21,
+        name: "Rasgulla Chaat",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 84,
+        img: rasgullaChat
+    },
+    {
+        id: 22,
+        name: "Samosa Chaat",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 72,
+        img: samosaChat
+    },
+    {
+        id: 23,
+        name: "Samosa Mutter",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 75,
+        img: samosaMutter
+    },
+    {
+        id: 24,
+        name: "Sev Puri",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 68,
+        img: sevPuri
+    },
+    {
+        id: 25,
+        name: "Tikkiya Mutter",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 75,
+        img: tikiyaMutter
+    },
+    {
+        id: 26,
+        name: "Tomato Chaat",
+        category: "chaats",
+        bestseller: false,
+        qty: "1 Plate",
+        price: 60,
+        img: tomatoChaat
+    },
+    {
+        id: 27,
+        name: "Cold Badam Milk",
+        category: "beverages",
+        bestseller: false,
+        qty: "250ml",
+        price: 65,
+        img: badamMilk
+    },
+    {
+        id: 28,
+        name: "Chaas",
+        category: "beverages",
+        bestseller: false,
+        qty: "250ml",
+        price: 60,
+        img: chaas
+    },
+    {
+        id: 29,
+        name: "Hot Badam Milk",
+        category: "beverages",
+        bestseller: false,
+        qty: "250ml",
+        price: 65,
+        img: hotBadamMilk
+    },
+    {
+        id: 30,
+        name: "Mango Lassi",
+        category: "beverages",
+        bestseller: false,
+        qty: "250ml",
+        price: 73,
+        img: mangoLassi
+    },
+    {
+        id: 31,
+        name: "Salt Lassi",
+        category: "beverages",
+        bestseller: false,
+        qty: "250ml",
+        price: 65,
+        img: saltLassi
+    },
+    {
+        id: 32,
+        name: "Sweet Lassi",
+        category: "beverages",
+        bestseller: false,
+        qty: "250ml",
+        price: 65,
+        img: sweetLassi
     },
 ];
 
